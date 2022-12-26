0684c99a58e57c598a9ffb7b01f8f132e1d94123
mayumar <maytalbada@gmail.com>
2022-12-26 18:21:18 +0100

    Ejercicio 4 p3 terminado

--- p3/ej4/ej4.c
+++ p3/ej4/ej4.c
@@ -5,16 +5,29 @@
 #include <errno.h>
 #include <string.h>
 
-sem_t sem;
+sem_t a;
+sem_t b;
 
 void * escribirA(void * p){
     int i;
 
     for(i=0; i<5; i++){
-
+        //printf("A: bloqueo a\n");
+        if(sem_wait(&a)!=0){
+            printf("sem_wait error\n");
+            printf("errno value= %d, definido como %s\n", errno, strerror(errno));
+            pthread_exit(NULL);
+        }
 
         printf("A");
         fflush(NULL);
+
+        if(sem_post(&b)!=0){
+            printf("sem_post error\n");
+            printf("errno value= %d, definido como %s\n", errno, strerror(errno));
+            pthread_exit(NULL);
+        }
+        //printf("A: he desbloqueado b\n");
     }
 
     pthread_exit(NULL);
@@ -24,8 +37,22 @@ void * escribirB(void * p){
     int i;
 
     for(i=0; i<5; i++){
+        //printf("B: bloqueo b\n");
+        if(sem_wait(&b)!=0){
+            printf("sem_wait error\n");
+            printf("errno value= %d, definido como %s\n", errno, strerror(errno));
+            pthread_exit(NULL);
+        }
+
         printf("B");
         fflush(NULL);
+
+        if(sem_post(&a)!=0){
+            printf("sem_post error\n");
+            printf("errno value= %d, definido como %s\n", errno, strerror(errno));
+            pthread_exit(NULL);
+        }
+        //printf("B: he desbloqueado a\n");
     }
 
     pthread_exit(NULL);
@@ -34,7 +61,17 @@ void * escribirB(void * p){
 int main(){
     pthread_t tid_a, tid_b;
 
-    sem_init(&sem, 0, 1);
+    if(sem_init(&a, 0, 0)!=0){
+        printf("sem_init error\n");
+        printf("errno value= %d, definido como %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    if(sem_init(&b, 0, 1)!=0){
+        printf("sem_init error\n");
+        printf("errno value= %d, definido como %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
 
     if(pthread_create(&tid_a, NULL, (void *) escribirA, NULL)){
         printf("Error creating thread\n");
